1e2de3924dd60e54a3b4a18e9be58b57435b67b5
布施 武 <tak.fus@gmail.com>
2020-12-16 10:04:31 +0900

    Checkerを使ったサンプルに修正

--- Test1/Test1.ps1
+++ Test1/Test1.ps1
@@ -1,11 +1,38 @@
 ﻿#
 # Script.ps1
 #
-[System.Reflection.Assembly]::LoadFile("H:\Develop\Visualstudio\repos\AutomationConnectIQ\Utility\bin\Debug\net5.0-windows\Utility.dll")
 
-$garmin = new-object AutomationConnectIQ.Lib.GarminSDK -Property @{
-	key = "H:\Develop\Garmin\key\developer_key"
+using namespace AutomationConnectIQ.Lib;
+
+# チェック用オブジェクトを生成
+# パラメータは、デベロッパーキーとモンキープロジェクトファイル
+$check = New-Object Checker -Property @{
+	Key = "H:\Develop\Garmin\key\developer_key"
+	Project = "H:\Develop\eclipse-workspace\DigiFuse\monkey.jungle"
 }
-$project = New-Object AutomationConnectIQ.Lib.Jungle("H:\Develop\eclipse-workspace\DigiFuse\monkey.jungle")
 
-$garmin.BuildProgram($project, $project.Devices[0])
+# fr45でビルドチェック処理を実行
+# シミュレーション処理はdelegate bool Simulation(string device, Simulator sim) となっている
+# のでそれに合わせてPowerShellのラムダ式にしてある
+$check.Check("fr45", {
+	#デバイス名とシミュレーターオブジェクトをパラメータで受け取る
+	Param($device, $simulator)
+
+	# GPS座標を、日本・皇居に設定
+	$simulator.SetGPSPosition(35.685233, 139.752485)
+	
+	# 時間ウィンドウを開いて、言語を英語にしてから所定時間に変更する
+	$time = $simulator.CreateTime()
+	$time.Open()
+	$time.Time = Get-Date "2020-1-1 13:00:00"
+	$simulator.SetLanguage([Simulator+Language]::English)
+	$time.Action([TimeSimulator+ExecuteType]::Start)
+
+	sleep -Milliseconds 500	# 時間シミュレーションを開始直後、画面が更新されるまで待つ
+
+	# 画面をキャプチャー
+	$bitmap = $simulator.Capture()
+	$bitmap.Save($device+".png")
+	$time.Close()
+	$true 	# 処理継続のためtrueを返す。これはデバイス名が指定されていない場合の連続処理と同じdelegateを採用しているためのおまじない
+})
